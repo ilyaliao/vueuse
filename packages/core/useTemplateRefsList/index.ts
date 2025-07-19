a601fe97ff2d89a2983c38a2e9635127b8247f22
IlyaL <ilyaliao324@gmail.com>
2025-07-19 09:22:24 +0800

    feat: mark useTemplateRefsList as deprecated

--- packages/core/useTemplateRefsList/index.ts
+++ packages/core/useTemplateRefsList/index.ts
@@ -5,6 +5,9 @@ export type TemplateRefsList<T> = T[] & {
   set: (el: object | null) => void
 }
 
+/**
+ * @deprecated This function will be removed in future version. Use Vue's built-in `useTemplateRef` instead.
+ */
 export function useTemplateRefsList<T = Element>(): Readonly<Ref<Readonly<TemplateRefsList<T>>>> {
   const refs = deepRef<unknown>([]) as Ref<TemplateRefsList<T>>
   refs.value.set = (el: object | null) => {
